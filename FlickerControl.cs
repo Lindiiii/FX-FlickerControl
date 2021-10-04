0bd2be5d16ce101918c92d110dcc7cb7bf651e14
Lindiiii <77248392+Lindiiii@users.noreply.github.com>
2021-10-04 13:27:29 -0500

    Add files via upload

---
+++ FlickerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerControl : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+    //public Renderer rend;
+
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());//minTime, maxTime
+        }
+    }
+
+    IEnumerator FlickeringLight() //float minWaitTime, float maxWaitTime
+    {
+        for (; ; )
+        {
+            //rend.enabled = false;
+            GetComponent<MeshRenderer>().enabled = false; //on
+            yield return new WaitForSeconds(0.4f);
+            GetComponent<MeshRenderer>().enabled = true; //off
+            yield return new WaitForSeconds(3.0f);
+        }
+
+
+    }
+}
